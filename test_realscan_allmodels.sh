160ebb890f64014ed89fe6b009c6705341197e5e
dohoon2045 <dhkim@ipis.cau.ac.kr>
2023-03-18 14:40:23 +0900

    puedgeformer

--- test_realscan_allmodels.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-conda activate pugcn
-
-# kitti
-bash test_realscan.sh pretrain/pugan-punet/ ./data/real_scan_kitti_pugcn 0 --model punet --upsampler original
-bash test_realscan.sh pretrain/pugan-mpu/  ./data/real_scan_kitti_pugcn 0 --model mpu --upsampler duplicate
-bash test_realscan.sh pretrain/pugan-pugan/ ./data/real_scan_kitti_pugcn 0 --model pugan --more_up 2
-bash test_realscan.sh pretrain/pugan-pugc/  ./data/real_scan_kitti_pugcn 0 --model pugcn --k 20
